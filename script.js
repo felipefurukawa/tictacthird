22f8b44d76c83e7cde86fd9f5f6a01393f0ffb5c
felipefurukawa <felpissss@gmail.com>
2023-10-26 15:43:55 -0300

    new title

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let playerText = document.getElementById('playerText')
+let boxes = Array.from(document.getElementsByClassName('dropBox'))
+let crosses = Array.from(document.getElementsByClassName('cross'))
+let circle = Array.from(document.getElementsByClassName('circle'))
+
+// console.log(boxes)
+// console.log(crosses)
+// console.log(circle)
+
+let currentPlayer = ''
+let spaces = Array(9).fill(null)
+
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('droped', drop))
+}
+
+
 function allowDrop(event){
     event.preventDefault();
 }
@@ -8,6 +25,8 @@ function drag(event){
 
 function drop(event){
     event.preventDefault();
+    const id = event.target.id
+
     var data = event.dataTransfer.getData("text");
     event.target.appendChild(document.getElementById(data));
 }
@@ -15,3 +34,7 @@ function drop(event){
 function reset(event){
     location.reload();
 }
+
+
+
+startGame()
